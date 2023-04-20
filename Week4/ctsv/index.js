59a2e44cf9bdc866907edd5117b0e59f307b9e8f
Nguyen Tuan Hiep <hiep454546@gmail.com>
2023-04-20 19:01:47 +0700

    final

--- Week4/ctsv/index.js
+++ Week4/ctsv/index.js
@@ -1,11 +1,25 @@
-function HidePersonalInfo() {
-  document.getElementById("personal-info").style.visibility = "hidden";
-  document.getElementById("ly-lich").style.color = "black";
-  document.getElementById("ly-lich").addEventListener("mouseover", () => { document.getElementById("ly-lich").style.color = "#409eff"; })
-  document.getElementById("ly-lich").addEventListener("mouseleave", () => { document.getElementById("ly-lich").style.color = "black"; });
-  document.getElementById("ly-lich").style.borderBottom = "1px solid rgb(235, 239, 243)";
+var tmp_id;
+var id_list = ['ly-lich', 'quyet-dinh', 'khen-thuong', 'lien-he', 'nhan-xet', 'hoat-dong-tham-gia'];
 
-  document.getElementById("tem").style.color = "#409eff";
+function HidePersonalInfo(id) {
+  document.getElementById("personal-info").style.visibility = "hidden";
+  document.getElementById("ly-lich").style.borderBottom = "1px solid rgb(235, 239, 243)";
+  if (id === tmp_id) return;
+  for (let idx = 0; idx < id_list.length; ++idx) {
+    if (id_list[idx] === id) {
+      console.log(1);
+      document.getElementById(id).style.color = "#409eff";
+      document.getElementById(id).addEventListener("mouseleave", () => { document.getElementById(id).style.color = "#409eff"; });
+    }
+    else {
+      console.log(0);
+      document.getElementById(id_list[idx]).style.color = "black";
+      document.getElementById(id_list[idx]).addEventListener("mouseover", () => { document.getElementById(id_list[idx]).style.color = "#409eff"; });
+      document.getElementById(id_list[idx]).addEventListener("mouseleave", () => { document.getElementById(id_list[idx]).style.color = "black"; });
+    }
+  }
+  document.getElementById(id).style.color = "#409eff";
+  tmp_id = id;
 }
 
 function ShowPersonalInfo() {
@@ -13,6 +27,10 @@ function ShowPersonalInfo() {
   document.getElementById("ly-lich").style.color = "#409eff";
   document.getElementById("ly-lich").style.borderBottom = "none";
   document.getElementById("ly-lich").addEventListener("mouseleave", () => { document.getElementById("ly-lich").style.color = "#409eff"; });
+  if (tmp_id) {
+    document.getElementById(tmp_id).style.color = "black";
+    tmp_id = 'ly-lich';
+  }
 }
 
 function ShowScoreStatus() {
@@ -20,7 +38,11 @@ function ShowScoreStatus() {
   document.getElementById("trang-thai").style.color = "#409eff";
   document.getElementById("trang-thai").style.borderBottom = "none";
   document.getElementById("trang-thai").addEventListener("mouseleave", () => { document.getElementById("trang-thai").style.color = "#409eff"; });
-
+  document.getElementById("tem").style.color = "black";
+  document.getElementById("tem").addEventListener("mouseover", () => { document.getElementById("tem").style.color = "#409eff"; });
+  document.getElementById("tem").addEventListener("mouseleave", () => { document.getElementById("tem").style.color = "black"; });
+  document.getElementById("tem").style.borderBottom = "1px solid rgb(235, 239, 243)";
+  if (tmp_id) document.getElementById(tmp_id).style.color = "#409eff";
 }
 
 function HideScoreStatus() {
@@ -28,9 +50,11 @@ function HideScoreStatus() {
   document.getElementById("trang-thai").style.color = "black";
   document.getElementById("trang-thai").addEventListener("mouseover", () => { document.getElementById("trang-thai").style.color = "#409eff"; })
   document.getElementById("trang-thai").addEventListener("mouseleave", () => { document.getElementById("trang-thai").style.color = "black"; });
-
-
   document.getElementById("trang-thai").style.borderBottom = "1px solid rgb(235, 239, 243)";
+  document.getElementById("tem").style.color = "#409eff";
+  document.getElementById("tem").style.borderBottom = "none";
+  document.getElementById("tem").addEventListener("mouseleave", () => { document.getElementById("tem").style.color = "#409eff"; });
+  if (tmp_id) document.getElementById(tmp_id).style.color = "#409eff";
 }
 
 ShowPersonalInfo();
