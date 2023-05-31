79651702af973ba3c65c9d8362a4b399aa1c9260
Nguyen Tuan Hiep <hiep454546@gmail.com>
2023-06-01 00:05:31 +0700

    :partying_face: fix bug

---
+++ Week8/chat-app/README.md
@@ -0,0 +1,14 @@
+# Socket.IO Chat App
+
+This is a simple chat application built using Socket.IO and Express. It allows users to select an available recipient and send messages in real-time.
+
+## Prerequisites
+
+- Node.js
+
+## How to run the application
+
+- Open the project folder
+- Run ```npm install``` to install the dependencies
+- Run ```npm start``` or ```npm run dev``` to start the server
+- Open your web browser and navigate to ```http://localhost:3000``` for each user to access the chat application
--- Week8/chat-app/server.js
+++ Week8/chat-app/server.js
@@ -1,108 +1,50 @@
-// const express = require("express");
-// const app = express();
-// const http = require("http").createServer(app);
-// const io = require("socket.io")(http);
-
-// app.use(express.static("public"));
-
-// app.get("/", (req, res) => {
-// 	res.sendFile(__dirname + "/index.html");
-// });
-
-// const users = new Map();
-
-// io.on("connection", (socket) => {
-// 	socket.on("setUsername", (username) => {
-// 		socket.username = username;
-// 		users.set(socket.id, username);
-// 		io.emit("userList", Array.from(users.values()));
-// 	});
-
-// 	socket.on("sendMessage", (data) => {
-// 		const { recipient, message } = data;
-// 		const sender = socket.username;
-// 		const recipientSocket = Array.from(users.entries()).find(
-// 			([id, name]) => name === recipient
-// 		)[0];
-// 		io.to(recipientSocket).emit("newMessage", { sender, message });
-// 	});
-
-// 	socket.on("disconnect", () => {
-// 		users.delete(socket.id);
-// 		io.emit("userList", Array.from(users.values()));
-// 	});
-
-// });
-
-// http.listen(3000, () => {
-// 	console.log("Server listening on http://localhost:3000");
-// });
-
-
-const express = require("express");
-const app = express();
-const http = require("http").createServer(app);
+const app = require("express")();
+const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
-app.use(express.static("public"));
+const { faker } = require("@faker-js/faker");
+
+const port = 3000;
+
+const connectedUsers = {};
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+	res.sendFile(__dirname + "/index.html");
 });
-
-const users = new Map();
-const chatHistory = {};
 
 io.on("connection", (socket) => {
-  socket.on("setUsername", (username) => {
-    socket.username = username;
-    users.set(socket.id, username);
-    io.emit("userList", Array.from(users.values()));
+	const username = faker.person.firstName();
+	connectedUsers[socket.id] = username;
+	socket.emit("username", username);
+	io.emit("userList", Object.values(connectedUsers));
+	socket.on("chat", ({ to, message }) => {
+		const from = connectedUsers[socket.id];
 
-    // Load chat history for the previously selected user (if any)
-    if (socket.selectedUser) {
-      loadChatHistory(socket, socket.selectedUser);
-    }
-  });
+		// Check if the sender and recipient are the same user
+		if (from === to) {
+			socket.emit("errorMessage", "You cannot chat with yourself.");
+			return;
+		}
 
-  socket.on("sendMessage", (data) => {
-    const { recipient, message } = data;
-    const sender = socket.username;
+		const recipientSocketId = Object.keys(connectedUsers).find(
+			(socketId) => connectedUsers[socketId] === to
+		);
 
-    // Store the message in chat history
-    if (!chatHistory[sender]) {
-      chatHistory[sender] = {};
-    }
-    if (!chatHistory[recipient]) {
-      chatHistory[recipient] = {};
-    }
-    chatHistory[sender][recipient] = chatHistory[sender][recipient] || [];
-    chatHistory[recipient][sender] = chatHistory[recipient][sender] || [];
-    chatHistory[sender][recipient].push({ sender, message });
-    chatHistory[recipient][sender].push({ sender, message });
+		if (recipientSocketId) {
+			io.to(recipientSocketId).emit("privateMsg", {
+				from,
+				message,
+			});
+		}
+	});
 
-    const recipientSocket = Array.from(users.entries()).find(
-      ([id, name]) => name === recipient
-    )[0];
-    io.to(recipientSocket).emit("newMessage", { sender, message });
-  });
-
-  socket.on("disconnect", () => {
-    users.delete(socket.id);
-    io.emit("userList", Array.from(users.values()));
-  });
+	// Event handler when a user disconnects
+	socket.on("disconnect", () => {
+		delete connectedUsers[socket.id];
+		io.emit("userList", Object.values(connectedUsers));
+	});
 });
 
-http.listen(3000, () => {
-  console.log("Server listening on http://localhost:3000");
+http.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}`);
 });
-
-function loadChatHistory(socket, selectedUser) {
-  const messages = chatHistory[socket.username][selectedUser];
-
-  if (messages) {
-    messages.forEach(({ sender, message }) => {
-      socket.emit("newMessage", { sender, message });
-    });
-  }
-}
